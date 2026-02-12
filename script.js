5c417fc853c8a4e7324a4eb8feb3c8d4fac9dbc5
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-02-12 23:35:22 +0330

    Reflector project structure

--- script.js
+++ script.js
@@ -7,30 +7,9 @@ let bodyColors = [
   "#ffa711",
 ];
 
-let hornsColors = [
-  "#a8d530",
-  "#42aaff",
-  "#f3d55b",
-  "#ff4f51",
-  "#904ae8",
-  "#ffa711",
-  "transparent",
-];
+const colors = [...bodyColors, "transparent"];
 
-let tailsColors = [
-  "#a8d530",
-  "#42aaff",
-  "#f3d55b",
-  "#ff4f51",
-  "#904ae8",
-  "#ffa711",
-  "transparent",
-];
-
-let faceCounter = 0;
-let colorCounter = 0;
-let hornCounter = 0;
-let tailCounter = 0;
+let [faceCounter, colorCounter, hornCounter, tailCounter] = Array(4).fill(0);
 
 const $ = document;
 const faceBtn = $.querySelector("#face-btn");
@@ -39,64 +18,39 @@ const hornsBtn = $.querySelector("#horns-btn");
 const tailBtn = $.querySelector("#tail-btn");
 const faceImg = $.querySelector("#face");
 
+const setCounter = (counter, length) => {
+  console.log(counter, length);
+  console.log(bodyColors.length);
+  return counter < length - 1 ? counter + 1 : 0;
+  // چرا اینجا counter++ جواب نمیده؟
+};
+
 // Change face
 faceBtn.addEventListener("click", () => {
-  faceCounter++;
-  if (faceCounter <= 5) {
-    faceImg.setAttribute("src", `face-${faceCounter}.png`);
-  } else {
-    faceCounter = 0;
-    faceImg.setAttribute("src", `face-${faceCounter}.png`);
-  }
+  faceCounter = setCounter(faceCounter, 6);
+  faceImg.setAttribute("src", `face-${faceCounter}.png`);
 });
 
 // Change color
 colorBtn.addEventListener("click", () => {
-  colorCounter++;
-  if (colorCounter <= bodyColors.length - 1) {
-    $.documentElement.style.setProperty(
-      "--color-character",
-      `${bodyColors[colorCounter]}`,
-    );
-  } else {
-    colorCounter = 0;
-    $.documentElement.style.setProperty(
-      "--color-character",
-      `${bodyColors[colorCounter]}`,
-    );
-  }
+  colorCounter = setCounter(colorCounter, bodyColors.length);
+  $.documentElement.style.setProperty(
+    "--color-character",
+    `${bodyColors[colorCounter]}`,
+  );
 });
 
 // Change horns
 hornsBtn.addEventListener("click", () => {
-  hornCounter++;
-  if (hornCounter <= hornsColors.length - 1) {
-    $.documentElement.style.setProperty(
-      "--color-horns",
-      `${hornsColors[hornCounter]}`,
-    );
-  } else {
-    hornCounter = 0;
-    $.documentElement.style.setProperty(
-      "--color-horns",
-      `${hornsColors[hornCounter]}`,
-    );
-  }
+  hornCounter = setCounter(hornCounter, colors.length);
+  $.documentElement.style.setProperty(
+    "--color-horns",
+    `${colors[hornCounter]}`,
+  );
 });
 
 // Change tails
 tailBtn.addEventListener("click", () => {
-  tailCounter++;
-  if (tailCounter <= tailsColors.length - 1) {
-    $.documentElement.style.setProperty(
-      "--color-tail",
-      `${tailsColors[tailCounter]}`,
-    );
-  } else {
-    tailCounter = 0;
-    $.documentElement.style.setProperty(
-      "--color-tail",
-      `${tailsColors[tailCounter]}`,
-    );
-  }
+  tailCounter = setCounter(tailCounter, colors.length);
+  $.documentElement.style.setProperty("--color-tail", `${colors[tailCounter]}`);
 });
